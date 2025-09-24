dec4472b7ee144916cd02094054f099ea118bafa
tanuki <mturu1976@gmail.com>
2025-09-24 11:58:51 +0900

    Fix network binding - listen on all interfaces (0.0.0.0) for Docker container networking

--- server.js
+++ server.js
@@ -25,6 +25,6 @@ app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
